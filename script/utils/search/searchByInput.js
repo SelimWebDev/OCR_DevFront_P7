031698ffe2a85f3aeffb8353b4ce8c18cae5f7d3
Selim El Mahi <selim.elmahi@gmail.com>
2022-03-22 18:20:46 +0100

    cleaning

--- script/utils/search/searchByInput.js
+++ script/utils/search/searchByInput.js
@@ -1,27 +1,20 @@
 import { filteredInputRecipes, initRecipes } from "../../state/state.js";
-import { isIncluded } from "./isIncluded.js";
 
 export function searchByInput(input){
     filteredInputRecipes.length = 0
 
     initRecipes.forEach(recipe => {
 
-        const recipeName = recipe.name.toLowerCase()
+        let match = false
+        if(recipe.name.toLowerCase().includes(input)){
+            match = true
+        } 
 
-        let recipeIngredient = []
         for(let i = 0; i < recipe.ingredients.length; i++){
             if(recipe.ingredients[i].ingredient.toLowerCase().includes(input)){
                 match = true
             } 
         }
-        // for(let i = 0; i < recipe.ustensils.length; i++){
-        //     if(recipe.ustensils[i].toLowerCase().includes(input)){
-        //         match = true
-        //     }
-        // }
-        // if(recipe.appliance.toLowerCase().includes(input)){
-        //     match = true
-        // }
 
         if(match){
             let alreadyExist = false
@@ -34,6 +27,5 @@ export function searchByInput(input){
                 filteredInputRecipes.push(recipe)
             }
         }
-        
     })
 }
